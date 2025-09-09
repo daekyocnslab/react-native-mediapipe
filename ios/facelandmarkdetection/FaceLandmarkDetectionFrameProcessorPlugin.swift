b8060bf1346f6210d81f2ffcff5fc6d14480c069
박봉석 <bongseok_park@daekyo.co.kr>
2025-09-09 15:23:38 +0900

    orientation 값 수정

--- ios/facelandmarkdetection/FaceLandmarkDetectionFrameProcessorPlugin.swift
+++ ios/facelandmarkdetection/FaceLandmarkDetectionFrameProcessorPlugin.swift
@@ -14,16 +14,9 @@ public class FaceLandmarkDetectionFrameProcessorPlugin: FrameProcessorPlugin {
     super.init(proxy: proxy, options: options)
   }
 
-  public override func callback(_ frame: Frame, withArguments arguments: [AnyHashable: Any]?) -> Any {
+  public override func callback(_ frame: Frame, withArguments arguments: [AnyHashable: Any]?) -> Any
+  {
     guard let detectorHandleValue = arguments?["detectorHandle"] as? Double else {
-      return false
-    }
-    // get the orientation argument. If its nil, return false
-    guard let orientation = arguments?["orientation"] as? String else {
-      return false
-    }
-    // convert the orientation string to a UIImage.Orientation
-    guard let uiOrientation = uiImageOrientation(from: orientation) else {
       return false
     }
 
@@ -35,7 +28,7 @@ public class FaceLandmarkDetectionFrameProcessorPlugin: FrameProcessorPlugin {
     let buffer = frame.buffer
     detector.detectAsync(
       sampleBuffer: buffer,
-      orientation: uiOrientation,
+      orientation: frame.orientation,
       timeStamps: Int(Date().timeIntervalSince1970 * 1000))
     return true
   }
